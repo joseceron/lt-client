06ccc46666881bcc53ee4632de066f57c5156d0c
Jose Ceron <joseluisceron13@gmail.com>
2022-11-23 16:54:20 -0500

    feat: bootstrap initialization

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,15 @@ import router from './router'
 
 import './assets/main.css'
 
+// Import Bootstrap and BootstrapVue CSS files (order is important)
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+// Make BootstrapVue available throughout your project
+Vue.use(BootstrapVue)
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
+
 new Vue({
   router,
   render: (h) => h(App)
